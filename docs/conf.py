0589b1ac400e00e7430f555db2071f6aa8ca38e8
Hagen Wierstorf <hwierstorf@audeering.com>
2021-04-28 11:19:33 +0200

    DOC: disable footer links

--- docs/conf.py
+++ docs/conf.py
@@ -53,6 +53,7 @@ html_theme = 'sphinx_audeering_theme'
 html_theme_options = {
     'display_version': True,
     'logo_only': False,
+    'footer_links': False,
 }
 html_context = {
     'display_github': True,
