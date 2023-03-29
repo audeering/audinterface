755b4d6179e81fb56526ed2b9edd36ce219ea0c3
Johannes Wagner <jwagner@audeering.com>
2023-03-29 09:58:43 +0200

    Fix: do not truncate string features (#110)
    * fix feature extraction when return value is a string
    
    * fix flake
    
    * TST: add test

--- audinterface/core/feature.py
+++ audinterface/core/feature.py
@@ -767,8 +767,6 @@ class Feature:
                 dtype=object,
             )
 
-        num = len(y)
-
         if (
                 self.win_dur is not None and
                 self.process_func_applies_sliding_window
@@ -825,17 +823,12 @@ class Feature:
 
                 index = utils.signal_index(starts, ends)
 
-            data = np.concatenate(data)
-
         else:
 
             index = y.index
-            dtype = self._values_to_frame(y[0]).dtype
-            shape = (num, len(self.column_names))
-            data = np.empty(shape, dtype)
+            data = [self._values_to_frame(values) for values in y]
 
-            for idx, values in enumerate(y):
-                data[idx, :] = self._values_to_frame(values)
+        data = np.concatenate(data)
 
         df = pd.DataFrame(
             data,
