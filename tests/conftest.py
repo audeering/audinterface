2e0512b5bda31e421370624a393b8d18de07ef15
Hagen Wierstorf <hwierstorf@audeering.com>
2023-07-28 14:56:50 +0200

    Introduce audio fixture

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pytest
+
+import audiofile
+import audmath
+
+
+@pytest.fixture(scope='module')
+def audio(tmpdir_factory, request):
+    """Fixture to generate audio file.
+
+    Provide ``(duration, sampling_rate)``
+    as parameter to this fixture.
+
+    """
+    file = str(tmpdir_factory.mktemp('audio').join('file.wav'))
+    duration, sampling_rate = request.param
+    signal = np.zeros((1, audmath.samples(duration, sampling_rate)))
+    audiofile.write(file, signal, sampling_rate)
+
+    yield file, signal, sampling_rate
