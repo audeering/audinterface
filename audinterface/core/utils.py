60c85fbe9f339d0dd5e7dc5c2117f3111757039d
Hagen Wierstorf <hwierstorf@audeering.com>
2023-07-28 11:45:48 +0200

    Clean up code

--- audinterface/core/utils.py
+++ audinterface/core/utils.py
@@ -464,13 +464,13 @@ def to_timedelta(
         ]
     else:
         # single duration entry
+
+        # avoid converting Timedelta values to ensure precision
+        # https://github.com/audeering/audinterface/pull/137
         if isinstance(durations, pd.Timedelta):
             return durations
 
         durations = duration_in_seconds(durations, sampling_rate)
         durations = pd.to_timedelta(durations, unit='s')
-
-    # if isinstance(durations, pd.TimedeltaIndex):
-    #     durations = list(durations)
 
     return durations
