Fix precision of end values in process_file() (#145)
* Fix precision of end values in process_file()

* Expand precision tests

* Introduce audio fixture

tests/test_process.py
@@ -8,7 +8,6 @@ import audeer
 import audformat
 import audiofile
 import audiofile as af
-import audmath
 import audobject
 
 import audinterface
@@ -1521,6 +1520,7 @@ def test_process_with_special_args(tmpdir):
     pd.testing.assert_series_equal(y, expected)
 
 
+@pytest.mark.parametrize('audio', [(3, 8000)], indirect=True)  # s, Hz
 @pytest.mark.parametrize(
     # `starts` and `ends`
     # are used to create a segment object
@@ -1531,16 +1531,22 @@ def test_process_with_special_args(tmpdir):
         (0, 1.5),
         (1.5, 3),
         ([0, 1.5], [1.5, 3]),
-        # Blocked by https://github.com/audeering/audinterface/issues/134
-        # or a similar issue
-        # ([0, 1.5], [1, 2.000000003]),
         ([0, 2], [1, 3]),
         ([0, 1], [2, 2]),
+        # https://github.com/audeering/audinterface/pull/145
+        ([0, 1.5], [1, 2.000000003]),
+        ([0.000000003, 1.5], [1, 2]),
+        ([1.000000003, 1.5], [1.1, 2]),
+        ([1.000000003, 2.1], [2.000000003, 2.5]),
         # https://github.com/audeering/audinterface/issues/135
         ([0, 1], [3, 2]),
     ]
 )
-def test_process_with_segment(tmpdir, starts, ends):
+def test_process_with_segment(audio, starts, ends):
+
+    path, signal, sampling_rate = audio
+    root, file = os.path.split(path)
+    duration = signal.shape[1] / sampling_rate
 
     # Segment and process objects
     segment = audinterface.Segment(
@@ -1549,20 +1555,6 @@ def test_process_with_segment(tmpdir, starts, ends):
     )
     process = audinterface.Process()
     process_with_segment = audinterface.Process(segment=segment)
-
-    # Create signal and file
-    sampling_rate = 8000
-    if ends is None:
-        duration = 1
-    else:
-        duration = audmath.duration_in_seconds(
-            max(audeer.to_list(ends))
-        )
-    signal = np.zeros((1, audmath.samples(duration, sampling_rate)))
-    root = tmpdir
-    file = 'file.wav'
-    path = os.path.join(root, file)
-    audiofile.write(path, signal, sampling_rate)
 
     # Expected index
     if starts is None:
@@ -1671,12 +1663,9 @@ def test_process_with_segment(tmpdir, starts, ends):
         ),
     )
 
-def test_read_audio(tmpdir):
-    sampling_rate = 8000
-    signal = np.ones((1, 8000))
-    path = str(tmpdir.mkdir('wav'))
-    file = os.path.join(path, 'file.wav')
-    af.write(file, signal, sampling_rate)
+@pytest.mark.parametrize('audio', [(1, 8000)], indirect=True)  # s, Hz
+def test_read_audio(audio):
+    file, _, sampling_rate = audio
     s, sr = audinterface.utils.read_audio(
         file,
         start=pd.Timedelta('00:00:00.1'),